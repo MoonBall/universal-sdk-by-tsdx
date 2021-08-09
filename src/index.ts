6b97a5af363f94fb185fe039082aaec7ce6af6a6
Gang Chen <gangc.cxy@foxmail.com>
2021-08-09 16:10:52 +0800

    use process.env.TARGET_ENVIRONMENT and dynamic require

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import './entry';
+// @ts-ignore
+import { testSha256 } from './use-crypto';
 
 export const sum = (a: number, b: number) => {
+  testSha256();
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
   }
   return a + b;
 };
+
+sum(1, 2);
--- tsdx.config.js
+++ tsdx.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const replace = require('@rollup/plugin-replace');
+const commonjs = require('@rollup/plugin-commonjs');
+const nodeResolve = require('@rollup/plugin-node-resolve');
 
 function p(relativeSrcPath) {
   return path.resolve(__dirname, 'src', relativeSrcPath);
@@ -16,6 +18,29 @@ module.exports = {
 
     if (options.target === 'browser') {
       config.input = p('browser-index.ts');
+
+      {
+        const resolverPluginIdx = config.plugins.findIndex(
+          it => it.name === 'node-resolve'
+        );
+        config.plugins[resolverPluginIdx] = nodeResolve.default({
+          browser: true,
+          mainFields: ['module', 'main'],
+          extensions: [...nodeResolve.DEFAULTS.extensions, '.jsx'],
+        });
+      }
+
+      if (options.format === 'umd') {
+        config.external = () => false;
+        config.plugins = config.plugins.filter(
+          it => it && it.name !== 'commonjs'
+        );
+        config.plugins.push(
+          commonjs({
+            include: [/use-crypto/, /\/node_modules\//],
+          })
+        );
+      }
     }
 
     return config; // always return a config.
